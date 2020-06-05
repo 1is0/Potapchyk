0d4980c0abfcc5e266cc9c749f02f7a63be5f237
WildRosa <62389101+WildRosa@users.noreply.github.com>
2020-06-05 23:51:59 +0300

    Add files via upload

---
+++ C#/Laboratory 8/Laba8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace laba8
+{
+    class Program
+    {
+        delegate string competition(string a, string b);
+        static void Main(string[] args)
+        {
+            string nameCompetition = "Olympic Games";
+            string reward = "Silver";
+            competition Outputcompetititon = (firstPart, secondPart) => firstPart + secondPart;
+            Competition sportsman = new Competition((Competition.Ranking)2, "Ivanov", 24, "Run", nameCompetition, reward, "Belarus", 12.3);
+            sportsman.ShowResults();
+            sportsman.Ev += AgeShow;
+            if (sportsman.Age < 19)
+            {
+                Console.WriteLine("\n\n");
+            }
+            else
+            {
+                sportsman.ShowAge();
+            }
+            Console.WriteLine(Outputcompetititon("Name Competition and Reward:", $" {nameCompetition}/{reward}"));
+            Console.ReadKey();
+        }
+        static void AgeShow(string age)
+        {
+            Console.WriteLine(age);
+        }
+
+    }
+ 
+}
+
---
+++ C#/Laboratory 8/Laba8/SportsmanResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba8
+{
+    interface SportsmanResult
+    {
+        public void Result();
+    }
+}
