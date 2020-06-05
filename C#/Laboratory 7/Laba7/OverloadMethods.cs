2756362f61bf8acab1f053499a3fa39729aefcb0
WildRosa <62389101+WildRosa@users.noreply.github.com>
2020-06-05 14:51:55 +0300

    Add files via upload

---
+++ C#/Laboratory 7/Laba7/OverloadMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laba7
+{
+    class OverloadMethods
+    {
+        public int number { get; set; }
+
+        public static int CheckInteger()
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+                Console.WriteLine("Enter the number again  ");
+            }
+            return a;
+        }
+
+        public static int CheckNatural()
+        {
+            int b;
+            while ((!int.TryParse(Console.ReadLine(), out b) || b <= 0))
+            {
+                Console.WriteLine("Enter the number again  ");
+            }
+            return b;
+        }
+
+        public static string StrRepresent(string znachenie, int a)
+        {
+            string s;
+            s = znachenie + $"={a}";
+            return s;
+        }
+
+        public static OverloadMethods operator +(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number + b.number };
+        }
+        public static OverloadMethods operator -(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number - b.number };
+        }
+        public static OverloadMethods operator *(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number * b.number };
+        }
+        public static OverloadMethods operator /(OverloadMethods a, OverloadMethods b)
+        {
+            return new OverloadMethods { number = a.number / b.number };
+        }
+        public static bool operator >(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number > b.number;
+        }
+        public static bool operator <(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number < b.number;
+        }
+
+        public static bool operator <=(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number <= b.number;
+        }
+
+        public static bool operator >=(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number >= b.number;
+        }
+        public static bool operator ==(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number == b.number;
+        }
+        public static bool operator !=(OverloadMethods a, OverloadMethods b)
+        {
+            return a.number != b.number;
+        }
+
+    }
+}
---
+++ C#/Laboratory 7/Laba7/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.WriteLine("Enter a integer number(n):");
+            n = OverloadMethods.CheckInteger();
+            Console.WriteLine("Enter a natural number(m):");
+            m = OverloadMethods.CheckNatural();
+            OverloadMethods N = new OverloadMethods { number = n };
+            OverloadMethods M = new OverloadMethods { number = m };
+            bool peer;
+            bool bm;
+            peer = N == M;
+            bm = N > M;
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Select an action: ");
+                Console.WriteLine("1.Addition");
+                Console.WriteLine("2.Subtraction");
+                Console.WriteLine("3.Multiplication");
+                Console.WriteLine("4.Division");
+                Console.WriteLine("5.Compare");
+                Console.WriteLine("6.Chech numbers for equality");
+                Console.WriteLine("7.Exit");
+                int otvet = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (otvet)
+                {
+                    case 1: Console.WriteLine($"N+M= {(N + M).number}"); break;
+                           
+                    case 2: Console.WriteLine($"N-M={(N - M).number}"); break;
+
+                    case 3:
+                        Console.WriteLine($"N*M={(N * M).number}"); break;
+                    case 4:
+                        Console.WriteLine($"N/M={(N / M).number}"); break;
+                    case 5:
+                        switch (bm)
+                        {
+                            case true:
+                                Console.WriteLine($"{n}>{m}");
+                                break;
+                            case false:
+                                Console.WriteLine($"{n}<{m}");
+                                break;
+                        }
+                        break;
+                    case 6:  switch (peer)
+                        {
+                            case true:
+                                Console.WriteLine($"{n}={m}");
+                                break;
+                            case false:
+                                Console.WriteLine($"{n}!={m}");
+                                break;
+                        }
+                     break;
+                    case 7: return;
+                    default:
+                        break;
+                } }
+       
+        }
+    }
+}
